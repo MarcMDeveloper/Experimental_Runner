acf9a08a02b44d66f6a2c81855488605b20367b7
Marc M <marcmirapeix@gmail.com>
2024-07-29 19:38:52 +0200

    PlayerBasic Movement

--- Unity_Experiment_Runner/Assets/Scripts/Player/PlayerController.cs
+++ Unity_Experiment_Runner/Assets/Scripts/Player/PlayerController.cs
@@ -5,42 +5,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
-    public float speed = 10f;
+    public float speed = 3000.0f;
     public Vector3 jump;
-    public float jumpForce = 2.0f;
+    public float jumpForce = 11.0f;
 
     public bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
-        jump = new Vector3(0.0f, 2.0f, 0.0f);        
+        jump = new Vector3(0.0f, 8.0f, 0.0f);
+        speed = 3000.0f;
+        isGrounded = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xMove = Input.GetAxisRaw("Horizontal"); 
-       
-        rb.velocity = new Vector3(xMove, rb.velocity.y, rb.velocity.z) * speed;
+        if (isGrounded)
+        {
+            float xMove = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector3(xMove * speed * Time.deltaTime, rb.velocity.y, 0);
+        }
+
+
+
 
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
 
-            rb.AddForce(jump , ForceMode.Impulse);
+            rb.AddForce(jump , ForceMode.VelocityChange);
             isGrounded = false;
-        }
-        
-        
+        }     
     }
 
-    
-    void OnCollisionStay(){
-        isGrounded = true;
-    }
 
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Death")
             Debug.Log("Death");
+        if(collision.gameObject.tag == "Ground")
+            isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = false;
     }
 
 }
